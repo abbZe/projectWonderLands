f53aed5b96f9d118de8d5c6625197e906fca2394
turboGraf <abbzew@gmail.com>
2023-02-22 20:10:50 +0200

    sidebar with selected links created

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,19 +1,37 @@
+import {NavLink} from "react-router-dom";
 import s from './SideBar.module.css';
+
+const selectedLink = () => {
+  return (
+    selected => selected.isActive ? s.active : undefined
+);
+}
 
 const SideBar = () => {
   return (
-    <aside className={s.sideBar}>
-      <nav>
-        <ul className={s.navList}>
-          <li className={s.navItem}><a href="/profile">Profile</a></li>
-          <li className={s.navItem}><a href="/dialogs">Dialogs</a></li>
-          <li className={s.navItem}><a href="/news">News</a></li>
-          <li className={s.navItem}><a href="/music">Music</a></li>
-          <li className={s.navItem}><a href="/settings">Settings</a></li>
-        </ul>
-      </nav>
-    </aside>
-  );
+  <aside className={s.sideBar}>
+    <nav>
+      <ul className={s.navList}>
+        <li className={s.navItem}>
+          <NavLink to="/profile" className={selectedLink()}>Profile</NavLink>
+        </li>
+        <li className={s.navItem}>
+          <NavLink to="/dialogs" className={selectedLink()}>Dialogs</NavLink>
+        </li>
+        <li className={s.navItem}>
+          <NavLink to="/news" className={selectedLink()}>News</NavLink>
+        </li>
+        <li className={s.navItem}>
+          <NavLink to="/music" className={selectedLink()}>Music</NavLink>
+        </li>
+        <li className={s.navItem}>
+          <NavLink to="/settings" className={selectedLink()}>Settings</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+)
+  ;
 }
 
 export default SideBar;
